b1c5671054ae1bf85566065e5f7c06d0996ee590
Dobbyyo <rlaekals9709@gmail.com>
2024-07-24 20:59:43 +0900

    merge를 위한 배포

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/GlobalStyle';
@@ -13,19 +12,19 @@ import GlobalStyle from './styles/GlobalStyle';
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-        <RecoilRoot>
-            <QueryClientProvider client={queryClient}>
-                <GlobalStyle />
-                <App />
-            </QueryClientProvider>
-        </RecoilRoot>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <App />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
